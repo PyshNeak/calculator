a672bcd84cc2da514ccbb09704b930ed3e0a8590
Max Pyshniak <maxpyshniak@gmail.com>
2024-12-03 10:40:46 -0700

    Fix divide by 0 bug

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const buttonContainer = document.querySelector('.buttons');
-const display = document.querySelector('.display p');
+const display = document.querySelector('.display');
+const ERROR_MSG = 'OOPS!';
 
 let v1 = null;
 let v2 = null;
@@ -14,13 +15,15 @@ buttonContainer.addEventListener('click', (e) => {
 });
 
 function onPress(val) {
-
     if (/\d/.test(val)) {
         runDigit(val);
     } else if (val === '.') {
         runDecimal();
     } else if (val === 'clear') {
         runClear();
+    } else if (display.textContent === ERROR_MSG) {
+        // Following functions invalid if error displayed
+        return;
     } else if (val === 'sign') {
         runSign();
     } else if (val === 'percentage') {
@@ -60,12 +63,12 @@ function runClear() {
 }
 
 function runSign() {
-    if (cleared) {
+    if (display.textContent === '0') {
         return;
     }
     if (display.textContent.charAt(0) === '-') {
         display.textContent = display.textContent.slice(1);
-    } else {
+    } else if (/\d/.test(display.textContent.charAt(0))) {
         display.textContent = "-" + display.textContent;
     }
 }
@@ -79,12 +82,13 @@ function runEquals() {
     if (v1 !== null && o1 !== null) {
         submitFunc();
         o1 = null;
+        cleared = true;
     }
 }
 
 function runOperator(val) {
     cleared = true
-    if (o1 === null) {
+    if (v1 === null || o1 === null) {
         v1 = display.textContent;
         o1 = val;
     } else {
@@ -102,9 +106,19 @@ function submitFunc() {
         res = +v1 - +v2;
     } else if (o1 === 'divide') {
         res = +v1 / +v2;
+        if (+v2 === 0) {
+            res = NaN;
+            console.log('what!');
+        }
     } else if (o1 === 'multiply') {
         res = +v1 * +v2;
     }
-    display.textContent = res;
-    v1 = display.textContent;
+
+    if (isNaN(res)) {
+        runClear();
+        display.textContent = 'OOPS!';
+    } else {
+        display.textContent = res;
+        v1 = display.textContent;
+    }
 }
